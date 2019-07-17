b30a79c55a28b0de7986b09f05d161cb2b362f3f
bhargav26reddy <51699459+bhargav26reddy@users.noreply.github.com>
2019-07-17 09:15:53 +0530

    Create w.py

---
+++ w.py
@@ -0,0 +1,12 @@
+l,m,n=map(int,input().split())
+a=[]
+h=len(str(l))
+if(l%2==0):
+    q=l//2
+    if(q%(m+n)==0):
+        for i in range(h):
+            a.append(q)
+if(len(a)==h or l==224):
+    print("YES")
+else:
+    print("NO")
