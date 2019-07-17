fb613b70af301c41fd37ddb375ee126fa1e8f813
bhargav26reddy <51699459+bhargav26reddy@users.noreply.github.com>
2019-07-17 09:02:50 +0530

    Create r.py

---
+++ r.py
@@ -0,0 +1,12 @@
+l,m,n=map(int,input().split())
+a=[]
+b=[]
+h=len(str(l))
+if(l%2==0):
+    q=l//2
+    for i in range(h):
+        a.append(q)
+if(len(a)==h):
+    print("YES")
+else:
+    print("NO")
